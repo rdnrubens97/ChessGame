496f7acc9926bc87114bef51371b03a675746c92
Rubens Dias <rubensdiasneto@gmail.com>
2023-01-19 13:11:46 -0300

    Criando a classe Posicao

--- chess-console/Program.cs
+++ chess-console/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Tabuleiro;
+
+Posicao ponto = new Posicao(2, 3);
+
+Console.WriteLine(ponto);
