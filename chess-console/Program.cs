c9e2098568acdd24b70a321f985f8019823f9cba
Rubens Dias <rubensdiasneto@gmail.com>
2023-01-20 13:55:22 -0300

    Criando classe PosicaoXadrez

--- chess-console/Program.cs
+++ chess-console/Program.cs
@@ -2,7 +2,12 @@
 using chess_console;
 using xadrez;
 
+PosicaoXadrez z = new PosicaoXadrez('c', 7);
+Console.WriteLine(z);
 
+Console.WriteLine(z.ToPosicao());
+
+/*
 try
 {
     Tabuleiro tab = new Tabuleiro(8, 8);
@@ -11,14 +16,11 @@ try
     tab.ColocarPeca(new Torre(tab, tabuleiro.Enums.Cor.Preta), new Posicao(1, 3));
     tab.ColocarPeca(new Rei(tab, tabuleiro.Enums.Cor.Preta), new Posicao(0, 0));
 
-
-
     Tela.ImprimirTabuleiro(tab);
-
 
 }
 catch (TabuleiroException e)
 {
     Console.WriteLine(e.Message);
-}
+}*/
 
