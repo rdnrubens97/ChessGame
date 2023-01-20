a79d313356aa0ef1db6cfbb6a6721451669dbb4a
Rubens Dias <rubensdiasneto@gmail.com>
2023-01-20 12:08:07 -0300

    Tratando erros com excecao personalizada

--- chess-console/Program.cs
+++ chess-console/Program.cs
@@ -2,17 +2,23 @@
 using chess_console;
 using xadrez;
 
-Tabuleiro tab = new Tabuleiro(8, 8);
 
-tab.ColocarPeca(new Torre(tab, tabuleiro.Enums.Cor.Preta), new Posicao(0, 0));
-tab.ColocarPeca(new Torre(tab, tabuleiro.Enums.Cor.Preta), new Posicao(1, 3));
-tab.ColocarPeca(new Rei(tab, tabuleiro.Enums.Cor.Preta), new Posicao(2, 4));
+try
+{
+    Tabuleiro tab = new Tabuleiro(8, 8);
 
-
-
-Tela.ImprimirTabuleiro(tab);
-
-Console.ReadLine();
+    tab.ColocarPeca(new Torre(tab, tabuleiro.Enums.Cor.Preta), new Posicao(0, 0));
+    tab.ColocarPeca(new Torre(tab, tabuleiro.Enums.Cor.Preta), new Posicao(1, 3));
+    tab.ColocarPeca(new Rei(tab, tabuleiro.Enums.Cor.Preta), new Posicao(0, 0));
 
 
+
+    Tela.ImprimirTabuleiro(tab);
+
+
+}
+catch (TabuleiroException e)
+{
+    Console.WriteLine(e.Message);
+}
 
