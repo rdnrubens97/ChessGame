32d2ad4485006e92955673eb55b93316ab970e92
Rubens Dias <rubensdiasneto@gmail.com>
2023-01-20 14:19:05 -0300

    Melhorando a implementacao do metodo imprimir tabuleiro

--- chess-console/Program.cs
+++ chess-console/Program.cs
@@ -2,19 +2,13 @@
 using chess_console;
 using xadrez;
 
-PosicaoXadrez z = new PosicaoXadrez('c', 7);
-Console.WriteLine(z);
-
-Console.WriteLine(z.ToPosicao());
-
-/*
 try
 {
     Tabuleiro tab = new Tabuleiro(8, 8);
 
     tab.ColocarPeca(new Torre(tab, tabuleiro.Enums.Cor.Preta), new Posicao(0, 0));
     tab.ColocarPeca(new Torre(tab, tabuleiro.Enums.Cor.Preta), new Posicao(1, 3));
-    tab.ColocarPeca(new Rei(tab, tabuleiro.Enums.Cor.Preta), new Posicao(0, 0));
+    tab.ColocarPeca(new Rei(tab, tabuleiro.Enums.Cor.Branca), new Posicao(0, 2));
 
     Tela.ImprimirTabuleiro(tab);
 
@@ -22,5 +16,5 @@ try
 catch (TabuleiroException e)
 {
     Console.WriteLine(e.Message);
-}*/
+}
 
--- chess-console/Tela.cs
+++ chess-console/Tela.cs
@@ -1,4 +1,5 @@
 ﻿using tabuleiro;
+using tabuleiro.Enums;
 
 namespace chess_console
 {
@@ -8,18 +9,36 @@ namespace chess_console
         {
             for (int i = 0; i < tab.Linhas; i += 1)
             {
+                Console.Write($"{8 - i} ");
                 for (int j = 0; j < tab.Colunas; j += 1)
                 {
+                    
                     if (tab.Peca(i, j) == null)
                     {
                         Console.Write("- ");
                     }
                     else
                     {
-                        Console.Write($"{tab.Peca(i, j)} ");
+                        ImprimirPeca(tab.Peca(i, j));
+                        Console.Write(" ");
                     }
                 }
                 Console.WriteLine();
+            }
+            Console.WriteLine("  a b c d e f g h");
+        }
+        public static void ImprimirPeca(Peca peca)
+        {
+            if (peca.Cor == Cor.Branca)
+            {
+                Console.WriteLine(peca);
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
             }
         }
     }
